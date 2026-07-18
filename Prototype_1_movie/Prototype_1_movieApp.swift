b429b9d2bccaef18921ec51552e599bc68d82df3
Aniket-pd <aniketprasad123@gmail.com>
2026-07-18 16:12:41 +0530

    Initial Commit

---
+++ Prototype_1_movie/Prototype_1_movieApp.swift
@@ -0,0 +1,17 @@
+//
+//  Prototype_1_movieApp.swift
+//  Prototype_1_movie
+//
+//  Created by Aniket prasad on 18/7/26.
+//
+
+import SwiftUI
+
+@main
+struct Prototype_1_movieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
